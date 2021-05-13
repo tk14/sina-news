b5c6ad2d4f95f8bff0cbc4b2b3a9a489af891ac5
tk14 <tk14@163.com>
2021-05-13 17:01:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const timeout = setTimeout(
 const feed = new Feed({
   title: '新浪新闻',
   description: '新浪全球实时财经新闻直播',
-  link: 'https://ruanyf.github.io/sina-news/',
+  link: 'https://tk14.github.io/sina-news/',
   language: 'zh-CN',
   generator: 'sina news feed generator',
   feedLinks: {
-    json: 'https://ruanyf.github.io/sina-news/rss.json',
-    rss: 'https://ruanyf.github.io/sina-news/rss.xml'
+    json: 'https://tk14.github.io/sina-news/rss.json',
+    rss: 'https://tk14.github.io/sina-news/rss.xml'
   },
 });
 
